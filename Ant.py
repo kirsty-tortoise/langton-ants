62eb6e3dc22d5eab3cc074fe36613f09ea3bd93b
Kirsten Land <kirstyland29@gmail.com>
2017-09-28 09:52:01 +0100

    Add start stop reset buttons

--- Ant.py
+++ Ant.py
@@ -14,6 +14,7 @@ def turn_anticlockwise(direction):
 class Application:
     def __init__(self, master, width, height):
         self.ant_control = AntControl(master, width, height)
+        self.controls = Controls(master, self.ant_control)
 
 class AntControl:
     def __init__(self, master, width, height):
@@ -24,21 +25,9 @@ class AntControl:
                                 height=self.height*self.square_size,
                                 width=self.width*self.square_size)
         self.canvas.pack(side=tk.LEFT)
-        rectangles = dict()
-        total_height = self.square_size*height
-        for i in range(width):
-            for j in range(height):
-                id_ = self.canvas.create_rectangle(i*self.square_size,
-                                                   total_height - (j+1)*self.square_size,
-                                                   (i+1)*self.square_size,
-                                                   total_height - j*self.square_size,
-                                                   fill="white",
-                                                   outline="")
-                rectangles[(i, j)] = id_
-        self.rectangles = rectangles
-        self.coord = (width//2, height//2)
-        self.direction = NORTH
-        self.blacks = set()
+        self.draw()
+        self.reset()
+        self.running = True
         self.update()
 
     def move(self, direction, coords):
@@ -75,13 +64,58 @@ class AntControl:
         if self.check_visible(new_coord):
             self.canvas.itemconfig(self.rectangles[new_coord], fill="red")
         self.coord = new_coord
-        self.master.after(10, self.update)
+        self.last_callback = self.master.after(10, self.update)
 
     def check_visible(self, coord):
         return 0 <= coord[0] < self.width and 0 <= coord[1] < self.height
 
+    def stop(self):
+        self.master.after_cancel(self.last_callback)
+        self.running = False
+    
+    def start(self):
+        if not self.running:
+            self.running = True
+            self.update()
+    
+    def reset(self):
+        self.coord = (self.width//2, self.height//2)
+        self.direction = NORTH
+        self.blacks = set()
+        self.redraw()
+    
+    def draw(self):
+        self.rectangles = dict()
+        total_height = self.square_size*self.height
+        for i in range(self.width):
+            for j in range(self.height):
+                id_ = self.canvas.create_rectangle(i*self.square_size,
+                                                   total_height - (j+1)*self.square_size,
+                                                   (i+1)*self.square_size,
+                                                   total_height - j*self.square_size,
+                                                   fill="white",
+                                                   outline="")
+                self.rectangles[(i, j)] = id_
+    
+    def redraw(self):
+        for i in range(self.width):
+            for j in range(self.height):
+                self.canvas.itemconfig(self.rectangles[(i, j)], fill=self.check_colour((i,j)))
+        self.canvas.itemconfig(self.rectangles[self.coord], fill="red")
+
+
 class Controls:
-    pass
+    def __init__(self, master, ant_control):
+        self.master = master
+        self.ant_control = ant_control
+        self.frame = tk.Frame(self.master)
+        self.frame.pack(side=tk.LEFT)
+        self.stop_button = tk.Button(self.frame, text="STOP", command=ant_control.stop)
+        self.stop_button.pack()
+        self.start_button = tk.Button(self.frame, text="START", command=ant_control.start)
+        self.start_button.pack()
+        self.reset_button = tk.Button(self.frame, text="RESET", command=ant_control.reset)
+        self.reset_button.pack()
 
 root = tk.Tk()
 app = Application(root, 50, 50)
